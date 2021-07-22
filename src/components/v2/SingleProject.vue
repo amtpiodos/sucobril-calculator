ca70cb4935f0a6b9a31db49f1facfb1078847710
amtpiodos <amtpiodos@gmail.com>
2021-07-22 21:05:28 +0700

    Latest WIP: Format project details in projects list

--- src/components/v2/SingleProject.vue
+++ src/components/v2/SingleProject.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="bg-gray-500 p-3 rounded-md" v-on:click="fetchProject(project_id, project_name, project_location)">
+        
+        <p class="text-center text-white font-bold"> {{ project_name }}</p>
+        <p class="text-center text-white text-xs mb-2">( {{ proj_type }} )</p>
         <img class="mb-2 object-scale-down h-48 w-full" src="../../assets/img/p2.jpeg">
-        <p class="text-center text-white font-bold"> {{ project_name }} </p>
-        <p class="text-center text-white"> {{ proj_type }} </p>
-        <p class="text-center text-white"> {{ project_location }} </p>
+        <p class="text-center text-white text-sm font-bold"> {{ project_location }} </p>
+        <!-- temporary: to retrieve data from db -->
+        <p class="text-center text-white text-xs"> x available units </p>
+        <p class="text-center text-white text-xs"> Price Range: Php 1M - 3M</p>
 
     </div>
 </template>
